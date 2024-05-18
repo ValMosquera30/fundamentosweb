402bac108763809af76aa769e5de74b239b70977
Valentina Mosquera <valentinam5020@gmail.com>
2024-05-17 20:35:55 -0500

    Ajustes al proyecto

--- index.js
+++ index.js
@@ -500,3 +500,29 @@ const db =[
         tamano: "Grande"
     }
 ]
+
+// FORMULARIO REGISTRO
+// Envío Formulario Contacto
+emailjs.init('RGKQdOEOFrKK_ymvsyUEm'); 
+const btn = document.getElementById('button-contacto');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const serviceID = 'default_service';
+    const templateID = 'template_y735lib';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Se envió el mensaje',
+                timer: 2500,
+                showConfirmButton: false
+            });
+            window.location = "contacto.html";
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+});
